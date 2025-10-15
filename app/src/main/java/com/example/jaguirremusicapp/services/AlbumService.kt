f387ea9413a2c6331e8bdfd331487bcbc8106848
josaPlus <josafat.saldana@hotmial.comm>
2025-10-14 22:03:16 -0600

    creacion de model y service

---
+++ app/src/main/java/com/example/jaguirremusicapp/services/AlbumService.kt
@@ -0,0 +1,15 @@
+package com.example.jaguirremusicapp.services
+
+import com.example.jaguirremusicapp.models.Album
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface AlbumService {
+    // OBTENER TODOS LOS ALBUMS
+    @GET("api/albums")
+    suspend fun getAllAlbums(): List<Album>
+
+    // OBTENER ALBUM POR ID
+    @GET("api/albums/{id}")
+    suspend fun getAlbumById(@Path("id") id: Int): Album
+}
